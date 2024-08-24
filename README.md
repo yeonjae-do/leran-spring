7f45579f68837a9a712346af18f16e4fba28edf9
yeonjae <holmanyjd@gmail.com>
2024-08-24 21:47:52 +0900

    기본세팅

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # leran-spring
+test
---
+++ src/test/java/com/spring/learn_spring_framework/LearnSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.learn_spring_framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
